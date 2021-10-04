4c8d9b42e771341d6eddd4cafb12886789365a3b
sunsiva <sivaprakasams@gmail.com>
2021-10-04 19:21:36 +0530

    Add project files.

---
+++ SumOfTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCodePblms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var response = SumOfTwo();
+            Console.WriteLine(response);
+        }
+
+        private static int[] SumOfTwo()
+        {
+            int[] output = new int[2];
+            
+            return output;
+
+        }
+    }
+}
